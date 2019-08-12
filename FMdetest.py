3109858ca89c8d5f6141f69acfac5869364fcb58
duhan <kiger-666@163.com>
2019-08-12 19:50:28 +0800

    last commit is not finish,this is the second time

--- FMdetest.py
+++ FMdetest.py
@@ -13,28 +13,34 @@ plot = plt.plot
 fft = fftpack.fft
 pi = np.pi
 
-N = 1024
-fc = 100e6
+Down_factor = 10
+N = 5120
+fc = 0
 fi = 44e3
-fs = 1e6
-index = 1
+fs = fi*1280
+index = 40000
 j = np.complex(0,1)
 
 n = np.arange(N)
 t = n/fs
-inSig = sin(2*pi*fi*t)+sin(3*pi*fi*t)+sin(5*pi*fi*t)
+inSig = cos(2*pi*fi*t)#+cos(3*pi*fi*t)
 x = cos(2*pi*fc*t+index*inSig)+j*sin(2*pi*fc*t+index*inSig)
 xdecimate = signal.decimate(x,Down_factor,ftype="fir")
 xangle = np.angle(x)
 xdata = np.gradient(xangle)
+xdata = np.unwrap(xdata)
 
-plt.subplot(2,1,1)
+plt.subplot(4,1,1)
 #plt.ylim(-0.02,0.02)
-plt.xlim(0,0.0002)
-
+#plt.xlim(0,0.0002)
 plot(t,xdata)
-plt.subplot(2,1,2)
+plt.subplot(4,1,2)
+plot(x.real)
+plt.subplot(4,1,3)
+plot(x.imag)
+
+plt.subplot(4,1,4)
 plot(t,inSig)
-plt.xlim(0,0.0002)
+#plt.xlim(0,0.0002)
 
 plt.show()
